49407d19947ff7c53ede085277eb4b315047eece
mhoff2016 <48568665+mhoff2016@users.noreply.github.com>
2021-03-15 16:44:58 -0400

    First upload of control code
    Change Input direction: line 57 false -> true
    Change Crank direction: line 58 false -> true

---
+++ InputCrankCode/InputCrankCode/InputCrankCode.ino
@@ -0,0 +1,65 @@
+void setup() {
+  // put your setup code here, to run once:
+  
+#define EN        8  
+
+//Direction pin
+#define In_DIR     5 
+#define Crank_DIR     6
+
+//Step pin
+#define In_STP     2
+#define Crank_STP     3 
+ 
+
+}
+
+void step(boolean dir, byte dirPin, byte stepperPin, int stps, int delayTime)
+
+{
+
+
+  digitalWrite(dirPin, dir);
+
+  delay(10);
+
+  for (int i = 0; i < stps; i++) {
+
+    digitalWrite(stepperPin, HIGH);
+
+    delayMicroseconds(delayTime); 
+
+    digitalWrite(stepperPin, LOW);
+
+    delayMicroseconds(delayTime); 
+
+ 
+
+  pinMode(X_DIR, OUTPUT); pinMode(X_STP, OUTPUT);
+
+  pinMode(Y_DIR, OUTPUT); pinMode(Y_STP, OUTPUT);
+
+ // pinMode(Z_DIR, OUTPUT); pinMode(Z_STP, OUTPUT);
+
+  pinMode(EN, OUTPUT);
+
+  digitalWrite(EN, LOW);
+
+}
+}
+void loop(){
+  //DRV8825
+int delayTimeIn=100; //Delay between each pause (uS)
+int delayTimeCrank=100; //Delay between each pause (uS)
+int stpsIn=250;// Steps to move
+int stpsCrank=350;// Steps to move
+
+  step(false, In_DIR, In_STP, stpsIn, delayTimeIn); //X, Clockwise
+  step(false, Crank_DIR, Crank_STP, stpsCrank, delayTimeCrank); //Y, Clockwise
+  // step(false, Z_DIR, Z_STP, stps); //Z, Clockwise
+
+  delay(100);
+
+  
+
+}
